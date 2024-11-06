1a2e274ca84ff515eab9090b2f67e54fd9f4b333
taha.sami <taha.sami@dreambigsemi.com>
2024-11-06 18:26:26 +0500

    assignment-6-part-1
    Signed-off-by: taha.sami <taha.sami@dreambigsemi.com>

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -7,43 +7,196 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
-#include <netinet/in.h> 
+#include <netinet/in.h>
 #include <sys/types.h>
 #include <syslog.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <pthread.h>
+#include <time.h>
+#include <stdbool.h>
+#include <sys/queue.h>
 
 #define PORT 9000
 #define CONNECTION 3
 #define FILE_TO_WRITE "/var/tmp/aesdsocketdata"
 #define BUFFER_SIZE 1024
+#define BEGIN 0
 
 
-int server_socket_fd,client_fd,file_fd;
+int server_socket_fd,file_fd;
+
+pthread_mutex_t file_mutex;
+volatile sig_atomic_t stop_flag = 0;
+
+
+// Structure for thread node
+struct thread_info_st{
+    pthread_t thread_id;
+    int client_fd;
+    int fd;
+    TAILQ_ENTRY(thread_info_st) entries;
+};
+
+TAILQ_HEAD(tailqhead, thread_info_st) queue_head;
 
 void close_everything(void){
         syslog(LOG_ERR,"Caught signal, exiting");
         close(server_socket_fd);
-        close(client_fd);
         remove(FILE_TO_WRITE);
+        pthread_mutex_destroy(&file_mutex); // Destroy the mutex
         closelog();
 }
 void signal_handler(int signo){
 
     if((signo == SIGINT) || (signo == SIGTERM)){
+        stop_flag = 1;
         close_everything();
+        struct thread_info_st *temp;
+
+        TAILQ_FOREACH(temp,&queue_head,entries){
+            pthread_join(temp->thread_id,NULL);
+            free(temp);
+        }
+
         exit(EXIT_SUCCESS);
     }
 }
 
+
+void *client_handler(void *args)
+{
+    struct thread_info_st *thread_data=(struct thread_info_st *)args;
+    int client_fd = thread_data->client_fd;
+    // free(args);
+    int bytes_read;
+    int bytes_read_to_send;
+    int used_size = 0 ;
+    
+    char *recv_buffer = calloc(1, BUFFER_SIZE);
+    
+    if(recv_buffer == NULL){
+
+            syslog(LOG_ERR,"Failed to alocate memory \n");
+            pthread_exit(NULL);
+        }
+        
+         while ((!stop_flag) && ((bytes_read = recv(client_fd,recv_buffer + used_size, BUFFER_SIZE, 0)) > 0))
+         {
+            if(bytes_read == -1)
+		    {
+			perror("recv error");
+			close_everything();
+			exit(-1);
+		    }
+            used_size += bytes_read ; 
+
+            if( bytes_read <= 0) 
+                break;
+
+            if (strchr(recv_buffer, '\n')) break;
+
+
+            recv_buffer = realloc(recv_buffer, BUFFER_SIZE + used_size);
+
+            if (recv_buffer == NULL)
+            {
+                syslog(LOG_ERR, "Realloc Failed Exiting");
+                close_everything();
+                free(recv_buffer);
+                exit(EXIT_FAILURE);
+            }
+
+         }
+
+
+            pthread_mutex_lock(&file_mutex);
+
+            lseek(file_fd, 0, SEEK_END);
+            if (write(thread_data->fd,recv_buffer,used_size) < 0){
+            syslog(LOG_ERR, "writing to file failed \n");
+            free(recv_buffer);
+            close_everything();
+            exit(EXIT_FAILURE);
+            }
+
+            pthread_mutex_unlock(&file_mutex);
+            //fsync(file_fd);
+
+
+            int file_length =  lseek(thread_data->fd, BEGIN,SEEK_END);
+            char *send_buffer = calloc(1,file_length);
+
+
+            if(send_buffer == NULL){
+
+                syslog(LOG_ERR , "Failed file sending buffer \n");
+                free(recv_buffer);
+                close_everything();
+            }
+
+            lseek(thread_data->fd,BEGIN,SEEK_SET);
+
+            pthread_mutex_lock(&file_mutex);
+
+            while (!stop_flag && (bytes_read_to_send = read(file_fd, send_buffer, used_size)) > 0) 
+            {
+                bytes_read_to_send = send(client_fd, send_buffer, bytes_read_to_send, 0);
+                if (bytes_read_to_send < 0) {
+                syslog(LOG_ERR, "Send error");
+                close_everything();
+                exit(EXIT_FAILURE);
+                }
+
+            }
+
+            pthread_mutex_unlock(&file_mutex);
+
+
+
+            free(recv_buffer);
+            free(send_buffer);
+            close(client_fd);
+           // TAILQ_REMOVE(&queue_head, thread_data, entries);
+    
+            pthread_exit(NULL);
+
+}
+
+// Thread function to periodically append timestamp
+void *timestamp_thread(void *arg) {
+    while (!stop_flag) {
+        pthread_mutex_lock(&file_mutex);
+
+        // Get current time
+        time_t now = time(NULL);
+        struct tm *time_info = localtime(&now);
+        char time_str[BUFFER_SIZE];
+        strftime(time_str, sizeof(time_str), "timestamp:%a, %d %b %Y %H:%M:%S\n", time_info);
+
+        // Write timestamp to file
+        lseek(file_fd, 0, SEEK_END);
+        write(file_fd, time_str, strlen(time_str));
+
+        pthread_mutex_unlock(&file_mutex);
+
+        // Sleep for 10 seconds, checking stop_flag during sleep
+        for (int i = 0; i < 10 && !stop_flag; i++) {
+            sleep(1);
+        }
+    }
+    pthread_exit(NULL);
+}
 int main (int argc, char *argv[]){
     //FILE *file;
     struct sockaddr_in server_addr = {0};
     struct sockaddr_in client_addr = {0};
-    int bytes_read;
+//    struct thread_info_st *temp_node = NULL;
+    //int bytes_read;
     char *ipv4_addr;
-    int bytes_read_to_send;
+   // int bytes_read_to_send;
 
+    pthread_mutex_init (&file_mutex,NULL);
+    TAILQ_INIT(&queue_head);
 
     openlog ("aesdsocket",LOG_PID | LOG_PERROR,LOG_USER);
     if ( (server_socket_fd = socket(AF_INET,SOCK_STREAM,0)) == -1 ){
@@ -107,7 +260,7 @@ int main (int argc, char *argv[]){
 		dup (0);
     }
 
-    file_fd = open(FILE_TO_WRITE, O_CREAT | O_RDWR, 0644);
+    file_fd = open(FILE_TO_WRITE, O_CREAT | O_RDWR , 0644);
 
         if(file_fd < 0){
             syslog(LOG_ERR,"Failed to open file");
@@ -124,84 +277,111 @@ int main (int argc, char *argv[]){
         return -1;
     }
 
-   
+    // Start timestamp thread
+    pthread_t timestamp_tid;
+    pthread_create(&timestamp_tid, NULL, timestamp_thread, NULL);
+
     while (1)
     {
-          socklen_t client_addr_len = sizeof(client_addr);
-          if ((client_fd = accept(server_socket_fd,(struct sockaddr *)&client_addr,&client_addr_len)) < 0)
+        socklen_t client_addr_len = sizeof(client_addr);
+         int client_fd = accept(server_socket_fd,(struct sockaddr *)&client_addr,&client_addr_len);
+          if (client_fd < 0)
           {
             syslog(LOG_ERR,"Failed to accept connection \n");
-            close_everything();
-            exit(EXIT_FAILURE);
+            continue;
           }
         
         ipv4_addr =inet_ntoa(client_addr.sin_addr);
         syslog(LOG_INFO,"Accepted connection from %s \n",ipv4_addr);
 
         // int buffer_size = BUFFER_SIZE;
-        int used_size = 0 ;
-        char *recv_buffer = calloc(1, BUFFER_SIZE);
+         // Create a new thread_info structure
+        struct thread_info_st *thread_new_entry = NULL;
+        thread_new_entry = (struct thread_info_st *) malloc(sizeof(struct thread_info_st));
+        TAILQ_INSERT_HEAD(&queue_head,thread_new_entry,entries);
+        
+        thread_new_entry->client_fd = client_fd;
+        thread_new_entry->fd = file_fd;
+
+
+        pthread_create(&(thread_new_entry->thread_id),NULL,client_handler,(void *)thread_new_entry);
+
+        TAILQ_FOREACH(thread_new_entry,&queue_head,entries){
+            pthread_join(thread_new_entry->thread_id,NULL);
+            if(stop_flag){
+                TAILQ_REMOVE(&queue_head,thread_new_entry,entries);
+                free(thread_new_entry);
+                thread_new_entry = NULL;
+
+            }
+        }
+        // int used_size = 0 ;
+        // char *recv_buffer = calloc(1, BUFFER_SIZE);
     
-        if(recv_buffer == NULL){
+        // if(recv_buffer == NULL){
 
-            syslog(LOG_ERR,"Failed to alocate memory \n");
-            close_everything();
-            exit(EXIT_FAILURE);
-        }
+        //     syslog(LOG_ERR,"Failed to alocate memory \n");
+        //     close_everything();
+        //     exit(EXIT_FAILURE);
+        // }
         
 
-        while ((bytes_read = recv(client_fd,recv_buffer + used_size, BUFFER_SIZE, 0)) > 0)
-        {
+        // while ((bytes_read = recv(client_fd,recv_buffer + used_size, BUFFER_SIZE, 0)) > 0)
+        // {
 
-            used_size += bytes_read ; 
+        //     used_size += bytes_read ; 
 
-            recv_buffer = realloc(recv_buffer, BUFFER_SIZE + used_size);
+        //     recv_buffer = realloc(recv_buffer, BUFFER_SIZE + used_size);
 
-            if (recv_buffer == NULL)
-            {
-                syslog(LOG_ERR, "Realloc Failed Exiting");
-                close_everything();
-                free(recv_buffer);
-                exit(EXIT_FAILURE);
-            }
+        //     if (recv_buffer == NULL)
+        //     {
+        //         syslog(LOG_ERR, "Realloc Failed Exiting");
+        //         close_everything();
+        //         free(recv_buffer);
+        //         exit(EXIT_FAILURE);
+        //     }
 
-            if (strchr(recv_buffer, '\n'))
-                break;
-        }   
+        //     if (strchr(recv_buffer, '\n'))
+        //         break;
+        // }   
 
-        if (write(file_fd,recv_buffer,used_size) < 0){
-            syslog(LOG_ERR, "writing to file failed \n");
-            free(recv_buffer);
-            close_everything();
-            exit(EXIT_FAILURE);
-        }
-            fsync(file_fd);
-            lseek(file_fd, 0, SEEK_SET);
+        // if (write(file_fd,recv_buffer,used_size) < 0){
+        //     syslog(LOG_ERR, "writing to file failed \n");
+        //     free(recv_buffer);
+        //     close_everything();
+        //     exit(EXIT_FAILURE);
+        // }
+        //     fsync(file_fd);
+        //     lseek(file_fd, 0, SEEK_SET);
         
 
-            char *send_buffer = calloc(1,used_size);
+            // char *send_buffer = calloc(1,used_size);
 
-            if(send_buffer == NULL){
+            // if(send_buffer == NULL){
 
-                syslog(LOG_ERR , "Failed file sending buffer \n");
-                free(recv_buffer);
-                close_everything();
-            }
+            //     syslog(LOG_ERR , "Failed file sending buffer \n");
+            //     free(recv_buffer);
+            //     close_everything();
+            // }
 
-            while ((bytes_read_to_send =read(file_fd,send_buffer,used_size)) > 0)
-            {
-                bytes_read_to_send = send(client_fd,send_buffer,bytes_read_to_send,0);
-                if (bytes_read_to_send < 0) {
-                syslog(LOG_ERR, "Send error");
-                close_everything();
-                exit(EXIT_FAILURE);
-            }
-            }   
+            // while ((bytes_read_to_send =read(file_fd,send_buffer,used_size)) > 0)
+            // {
+            //     bytes_read_to_send = send(client_fd,send_buffer,bytes_read_to_send,0);
+            //     if (bytes_read_to_send < 0) {
+            //     syslog(LOG_ERR, "Send error");
+            //     close_everything();
+            //     exit(EXIT_FAILURE);
+            // }
+            // }   
 
-            syslog(LOG_INFO, "CLosed connection from %s \n",ipv4_addr);
+            // syslog(LOG_INFO, "CLosed connection from %s \n",ipv4_addr);
 
-            free(send_buffer);
-            free(recv_buffer);
-    }
+    //         free(send_buffer);
+    //         free(recv_buffer);
+     }
+     // Join timestamp thread on exit
+    pthread_join(timestamp_tid, NULL);
+    pthread_mutex_destroy(&file_mutex);
+    close_everything();
     return 0;
 }
